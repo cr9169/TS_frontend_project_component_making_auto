5bd23cab1dc137d04f0a5376fe318668c964b529
cr9169 <bar.gabay.home@gmail.com>
2023-03-05 11:12:53 +0200

    feat: added geters and setters to UIConsoleHandler, planning photo changed

--- UIConsoleHandler.py
+++ UIConsoleHandler.py
@@ -11,3 +11,15 @@ class UIConsoleHandler:
         self.depth = 0 
         # will get set after the user is done giving components names (input)
         self.fileCreator = FileCreator(components_names)
+
+    def get_components_names(self):
+        return self.components_names
+    
+    def set_components_names(self, new_names):
+        self.components_names = new_names
+    
+    def get_depth(self):
+        return self.depth
+    
+    def set_depth(self, new_depth):
+        self.depth = new_depth
