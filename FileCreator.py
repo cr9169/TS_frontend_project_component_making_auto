3d2c2f21183dc13ba6411e9fd3ee7639d6579e8a
cr9169 <bar.gabay.home@gmail.com>
2023-03-05 13:55:26 +0200

    feat: FileCreator is done

--- FileCreator.py
+++ FileCreator.py
@@ -1,6 +1,6 @@
 class FileCreator:
 
-    component_base_template = 'const X = ({{}}:{{}}) =>     { return (); };  export default X;'
+    component_base_template = "const X = ({{}}:{{}}) =>\n{\n return (); \n};\n  export default X;"
 
     def __init__(self, components_files_names):
         self.components_files_names = components_files_names
@@ -10,3 +10,15 @@ class FileCreator:
     
     def set_components_files_names(self, new_names):
         self.components_files_names = new_names
+
+    def adapt_component_base_template(component_name):
+        adapted_component_base_template = FileCreator.component_base_template.replace("X", component_name)
+        return adapted_component_base_template
+
+    def create_jsx_file(self, component_name):
+        with open(f"{component_name}.jsx", "w") as file:
+            file.write(self.adapt_component_base_template(component_name)) 
+
+    def create_scss_file(component_name):
+        with open(f"{component_name}.scss", "r"):
+            pass 
