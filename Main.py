ffb7c58a3e11e3922a1fc74532af6b08ae368992
cr9169 <bar.gabay.home@gmail.com>
2023-04-30 11:28:28 +0300

    feat: main handling

---
+++ Main.py
@@ -0,0 +1,8 @@
+from UIConsoleHandler import UIConsoleHandler
+
+def main():
+    ui_console_handler = UIConsoleHandler()
+    ui_console_handler.console_handling()
+
+if __name__ == "__main__":
+    main()
